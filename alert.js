6c0c0c39095b74deb18b971a5192f84348d7443f
unknown <kabuikariuki052@gmail.com>
2021-03-09 01:01:02 -0800

    created a program to calculate age

---
+++ alert.js
@@ -0,0 +1,7 @@
+let yob=1999
+let currentyear=2010
+let age=currentyear-yob
+
+
+console.log(age);
+
---
+++ volume.js
@@ -0,0 +1,12 @@
+
+function age(yob, currentyear){
+    let age=currentyear-yob
+
+    return age;
+    
+    
+}
+
+console.log(age(1997, 2020)) 
+
+console.log(age(1995, 2020))
